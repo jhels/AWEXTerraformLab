d3b0305d67656001567bf6624f2f854a71efbf0e
Andy Payne <andy.payne@airwalkconsulting.com>
2023-04-18 15:43:11 +0100

    update repo in userdata

--- site_files/instanceUserData.sh
+++ site_files/instanceUserData.sh
@@ -2,7 +2,7 @@
 yum -y update
 yum -y install httpd
 sudo yum install git -y
-git clone git clone https://github.com/PAYNEA03/nestedJenkins1.git
+git clone https://github.com/PAYNEA03/AWEXTerraformLab.git
 cp /AWEXTerraformLab/site_files/index.html /var/www/html/
 service httpd start
 chkconfig httpd on
